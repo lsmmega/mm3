493ebf43d1ed137302011708456169873bea3c1e
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-18 16:03:36 +0800

    Update sfx

---
+++ audio/sfx/explosion.asm
@@ -0,0 +1,30 @@
+sfx_explosion:
+;1
+	sfx_priority $11
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $96
+	sfx_global_transpose_note = $00
+	sfx_frames $05
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $28
+	sfx_noise_volume $f
+	sfx_noise_note 2
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $28
+	sfx_global_transpose_note = $00
+	sfx_frames $28
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $09
+	sfx_noise_command_flags_bits = $09
+	sfx_noise_instrument $30
+	sfx_noise_pitch_slide $aa
+	sfx_noise_note 2
+	sfx_end
