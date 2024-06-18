d549da2d4c77213f0a7289908e9fa0aa7b026e6f
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-18 10:05:47 +0800

    Update sfx

---
+++ audio/sfx/wanaan.asm
@@ -0,0 +1,52 @@
+sfx_wanaan:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $e
+	sfx_channel_flags_bits = $e
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $2c
+	sfx_pulse2_duty_cycle 3
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $7f
+	sfx_pulse2_note D#3
+	sfx_triangle_command_flags $0d
+	sfx_triangle_command_flags_bits = $0d
+	sfx_triangle_instrument $2b
+	sfx_triangle_fraction $8c
+	sfx_triangle_pitch_slide $7f
+	sfx_triangle_note A_7_
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $32
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $7f
+	sfx_noise_note 15
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $e
+	sfx_channel_flags_bits = $e
+	sfx_pulse2_command_flags $08
+	sfx_pulse2_command_flags_bits = $08
+	sfx_pulse2_pitch_slide $ff
+	sfx_pulse2_note E_6_
+	sfx_triangle_command_flags $0c
+	sfx_triangle_command_flags_bits = $0c
+	sfx_triangle_fraction $00
+	sfx_triangle_pitch_slide $ff
+	sfx_triangle_note A_7_
+	sfx_noise_command_flags $08
+	sfx_noise_command_flags_bits = $08
+	sfx_noise_pitch_slide $ff
+	sfx_noise_note 15
+	sfx_end
