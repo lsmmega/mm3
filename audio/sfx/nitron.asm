37fd118d3fd9df998d21d7f07a472905c79c11aa
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-18 11:10:58 +0800

    Update sfx

---
+++ audio/sfx/nitron.asm
@@ -0,0 +1,21 @@
+sfx_nitron:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $1e
+	sfx_channel_flags $c
+	sfx_channel_flags_bits = $c
+	sfx_triangle_command_flags $05
+	sfx_triangle_command_flags_bits = $05
+	sfx_triangle_instrument $34
+	sfx_triangle_fraction $ff
+	sfx_triangle_note A#3
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $28
+	sfx_noise_volume $f
+	sfx_noise_note 0
+	sfx_end
