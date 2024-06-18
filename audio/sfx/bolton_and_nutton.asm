af58eb37935d3ffeac218510221eab05798954bc
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-18 11:25:46 +0800

    Update sfx

---
+++ audio/sfx/bolton_and_nutton.asm
@@ -0,0 +1,24 @@
+sfx_bolton_and_nutton:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $0c
+	sfx_channel_flags $6
+	sfx_channel_flags_bits = $6
+	sfx_pulse2_command_flags $1f
+	sfx_pulse2_command_flags_bits = $1f
+	sfx_pulse2_instrument $32
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $ff
+	sfx_pulse2_pitch_tune $ff
+	sfx_pulse2_note A_6_
+	sfx_triangle_command_flags $05
+	sfx_triangle_command_flags_bits = $05
+	sfx_triangle_instrument $32
+	sfx_triangle_fraction $74
+	sfx_triangle_note A_7_
+	sfx_end
