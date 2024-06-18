3ddfd031ed78e463c026d0ea848666bf90d4bdd5
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-18 08:49:00 +0800

    Update sfx

---
+++ audio/sfx/unused21.asm
@@ -0,0 +1,42 @@
+sfx_unused21:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $4
+	sfx_channel_flags_bits = $4
+	sfx_triangle_command_flags $05
+	sfx_triangle_command_flags_bits = $05
+	sfx_triangle_instrument $29
+	sfx_triangle_fraction $9f
+	sfx_triangle_note A_7_
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $4
+	sfx_channel_flags_bits = $4
+	sfx_triangle_command_flags $04
+	sfx_triangle_command_flags_bits = $04
+	sfx_triangle_fraction $00
+	sfx_triangle_note A_7_
+
+:3
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $4
+	sfx_channel_flags_bits = $4
+	sfx_triangle_command_flags $04
+	sfx_triangle_command_flags_bits = $04
+	sfx_triangle_fraction $00
+	sfx_triangle_note A_7_
+	sfx_end
