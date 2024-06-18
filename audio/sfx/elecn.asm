29591ed0f55b61ca921534124db8740360d86810
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-18 14:09:28 +0800

    Update sfx

---
+++ audio/sfx/elecn.asm
@@ -0,0 +1,21 @@
+sfx_elecn:
+;1
+	sfx_priority $09
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $06
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $05
+	sfx_pulse2_command_flags_bits = $05
+	sfx_pulse2_instrument $32
+	sfx_pulse2_volume $f
+	sfx_pulse2_note A#5
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $32
+	sfx_noise_volume $c
+	sfx_noise_note 5
+	sfx_end
