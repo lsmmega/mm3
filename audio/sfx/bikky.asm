58830fa293ff6abf5c13152f7745b8640560fb78
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-18 08:12:53 +0800

    Update sfx

---
+++ audio/sfx/bikky.asm
@@ -0,0 +1,45 @@
+sfx_bikky:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $51
+	sfx_global_transpose_note = $00
+	sfx_frames $08
+	sfx_channel_flags $c
+	sfx_channel_flags_bits = $c
+	sfx_triangle_command_flags $0d
+	sfx_triangle_command_flags_bits = $0d
+	sfx_triangle_instrument $31
+	sfx_triangle_fraction $ff
+	sfx_triangle_pitch_slide $7f
+	sfx_triangle_note G_4
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $28
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $ff
+	sfx_noise_note 3
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $06
+	sfx_channel_flags $6
+	sfx_channel_flags_bits = $6
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $2c
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $7f
+	sfx_pulse2_note G#2
+	sfx_triangle_command_flags $0d
+	sfx_triangle_command_flags_bits = $0d
+	sfx_triangle_instrument $2c
+	sfx_triangle_fraction $ff
+	sfx_triangle_pitch_slide $ff
+	sfx_triangle_note G#3
+	sfx_end
