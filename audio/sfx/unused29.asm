42bcfdcdc753aaaa1e23d3f2b45d353e1b00eb97
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-19 09:41:27 +0800

    Update sfx

---
+++ audio/sfx/unused29.asm
@@ -0,0 +1,137 @@
+sfx_unused29:
+;1
+	sfx_priority $11
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $08
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $2f
+	sfx_noise_volume $f
+	sfx_noise_note 2
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $00
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+
+;3
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $08
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+
+;4
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $00
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+
+;5
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $00
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+
+;6
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $00
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+
+;7
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $00
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+
+;8
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $00
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+
+;9
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $00
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+
+;10
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $00
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+
+;11
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $00
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+
+;12
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $eb
+	sfx_global_transpose_note = $00
+	sfx_frames $00
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_volume $0
+	sfx_noise_note 2
+	sfx_end
