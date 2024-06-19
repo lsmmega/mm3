10b593b2a54ab9e4bcfb532f13cad69de339a4f0
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-19 10:27:09 +0800

    Update sfx

---
+++ audio/sfx/magnet.asm
@@ -0,0 +1,35 @@
+sfx_magnet:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ab
+	sfx_global_transpose_note = $00
+	sfx_frames $03
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $05
+	sfx_pulse2_command_flags_bits = $05
+	sfx_pulse2_instrument $33
+	sfx_pulse2_volume $f
+	sfx_pulse2_note C_2
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $33
+	sfx_noise_volume $f
+	sfx_noise_note 7
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $a9
+	sfx_global_transpose_note = $00
+	sfx_frames $25
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $09
+	sfx_noise_command_flags_bits = $09
+	sfx_noise_instrument $38
+	sfx_noise_pitch_slide $2c
+	sfx_noise_note 12
+	sfx_end
