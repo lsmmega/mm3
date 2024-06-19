e5f1779057183e2cc42029c577bea26af48b420c
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-19 10:34:29 +0800

    Update sfx

---
+++ audio/sfx/gemini.asm
@@ -0,0 +1,24 @@
+sfx_gemini:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $17
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $17
+	sfx_pulse2_command_flags_bits = $17
+	sfx_pulse2_instrument $29
+	sfx_pulse2_duty_cycle 0
+	sfx_pulse2_volume $6
+	sfx_pulse2_pitch_tune $02
+	sfx_pulse2_note G#3
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $32
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $01
+	sfx_noise_note 12
+	sfx_end
