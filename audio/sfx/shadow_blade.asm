b2cdb65b0a97605dd7befc1755794f586c88d784
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-19 15:09:10 +0800

    Update sfx

---
+++ audio/sfx/shadow_blade.asm
@@ -0,0 +1,23 @@
+sfx_shadow_blade:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $1b
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0d
+	sfx_pulse2_command_flags_bits = $0d
+	sfx_pulse2_instrument $32
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $88
+	sfx_pulse2_note D#5
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $32
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $98
+	sfx_noise_note 6
+	sfx_end
