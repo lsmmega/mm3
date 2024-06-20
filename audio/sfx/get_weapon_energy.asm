fa172fed50832192999cc1613387d36232b8a9a3
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-20 09:49:05 +0800

    Update sfx

---
+++ audio/sfx/get_weapon_energy.asm
@@ -0,0 +1,18 @@
+sfx_get_weapon_energy:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $e1
+	sfx_global_transpose_note = $00
+	sfx_frames $32
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $39
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $8f
+	sfx_pulse2_note C#5
+	sfx_end
