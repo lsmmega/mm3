ca7d59fed1c4ccfd5c8a17fa7c291680df1c9745
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-20 09:05:02 +0800

    Update sfx

---
+++ audio/sfx/hard_smash.asm
@@ -0,0 +1,50 @@
+sfx_hard_smash:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $10
+	sfx_channel_flags $e
+	sfx_channel_flags_bits = $e
+	sfx_pulse2_command_flags $0d
+	sfx_pulse2_command_flags_bits = $0d
+	sfx_pulse2_instrument $35
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $63
+	sfx_pulse2_note G_5
+	sfx_triangle_command_flags $0d
+	sfx_triangle_command_flags_bits = $0d
+	sfx_triangle_instrument $28
+	sfx_triangle_fraction $e6
+	sfx_triangle_pitch_slide $7f
+	sfx_triangle_note G_4
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $32
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $2e
+	sfx_noise_note 2
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $0a
+	sfx_channel_flags $e
+	sfx_channel_flags_bits = $e
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $5
+	sfx_pulse2_note D_7_
+	sfx_triangle_command_flags $04
+	sfx_triangle_command_flags_bits = $04
+	sfx_triangle_fraction $a1
+	sfx_triangle_note C_4
+	sfx_noise_command_flags $04
+	sfx_noise_command_flags_bits = $04
+	sfx_noise_volume $5
+	sfx_noise_note 2
+	sfx_end
