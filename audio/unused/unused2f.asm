42dd447e32ff713b47bd38caae025c2e92f18df8
lsmmega <pogmegalsm@outlook.com>
2024-06-22 17:34:03 +0800

    Create unused2f.asm

---
+++ audio/unused/unused2f.asm
@@ -0,0 +1,38 @@
+sfx_unused2f:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $6e
+	sfx_global_transpose_note = $00
+	sfx_frames $04
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $2c
+	sfx_pulse2_duty_cycle 0
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $a1
+	sfx_pulse2_note B_2
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $30
+	sfx_noise_volume $f
+	sfx_noise_pitch_slide $93
+	sfx_noise_note 6
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $28
+	sfx_global_transpose_note = $00
+	sfx_frames $28
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $09
+	sfx_noise_command_flags_bits = $09
+	sfx_noise_instrument $37
+	sfx_noise_pitch_slide $9c
+	sfx_noise_note 1
+	sfx_end
