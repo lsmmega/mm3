ccdd5389242daacdacd4e4ce2af3c6dad50c6ae5
lsmmega <pogmegalsm@outlook.com>
2024-11-26 21:43:12 +0800

    stages split

---
+++ stages.asm
@@ -0,0 +1,53 @@
+.SEGMENT "STAGE00"
+.INCBIN "stages/needle.bin"
+
+.SEGMENT "STAGE01"
+.INCBIN "stages/magnet.bin"
+
+.SEGMENT "STAGE02"
+.INCBIN "stages/gemini.bin"
+
+.SEGMENT "STAGE03"
+.INCBIN "stages/hard.bin"
+
+.SEGMENT "STAGE04"
+.INCBIN "stages/top.bin"
+
+.SEGMENT "STAGE05"
+.INCBIN "stages/snake.bin"
+
+.SEGMENT "STAGE06"
+.INCBIN "stages/spark.bin"
+
+.SEGMENT "STAGE07"
+.INCBIN "stages/shadow.bin"
+
+.SEGMENT "STAGE08"
+.INCBIN "stages/doc_needle.bin"
+
+.SEGMENT "STAGE09"
+.INCBIN "stages/doc_gemini.bin"
+
+.SEGMENT "STAGE0A"
+.INCBIN "stages/doc_spark.bin"
+
+.SEGMENT "STAGE0B"
+.INCBIN "stages/doc_shadow.bin"
+
+.SEGMENT "STAGE0C"
+.INCBIN "stages/wily1.bin"
+
+.SEGMENT "STAGE0D"
+.INCBIN "stages/wily2.bin"
+
+.SEGMENT "STAGE0E"
+.INCBIN "stages/wily3.bin"
+
+.SEGMENT "STAGE0F"
+.INCBIN "stages/wily4.bin"
+
+.SEGMENT "STAGE10"
+.INCBIN "stages/wily5.bin"
+
+.SEGMENT "STAGE11"
+.INCBIN "stages/wily6.bin"
