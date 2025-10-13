8b55bddc1373a5776b3beeedbb07779cabb5b438
lsmmega <pogmegalsm@outlook.com>
2025-10-13 15:46:49 +0800

    Create flags.asm

---
+++ constants/flags.asm
@@ -0,0 +1,25 @@
+;doc stage clear flags.
+	doc_stage_needleman_clear = 1 << 0
+	doc_stage_geminiman_clear = 1 << 2
+	doc_stage_sparkman_clear  = 1 << 6
+	doc_stage_shadowman_clear = 1 << 7
+
+;rematch flags.
+	rematch_needleman_done = 1 << 0
+	rematch_magnetman_done = 1 << 1
+	rematch_geminiman_done = 1 << 2
+	rematch_hardman_done   = 1 << 3
+	rematch_topman_done    = 1 << 4
+	rematch_snakeman_done  = 1 << 5
+	rematch_sparkman_done  = 1 << 6
+	rematch_shadowman_done = 1 << 7
+
+;regular stage clear flags.
+	stage_needleman_clear = 1 << 0
+	stage_magnetman_clear = 1 << 1
+	stage_geminiman_clear = 1 << 2
+	stage_hardman_clear   = 1 << 3
+	stage_topman_clear    = 1 << 4
+	stage_snakeman_clear  = 1 << 5
+	stage_sparkman_clear  = 1 << 6
+	stage_shadowman_clear = 1 << 7
