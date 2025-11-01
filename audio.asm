7213adc617472f65fc01d490442434afc0335289
lsmmega <pogmegalsm@outlook.com>
2025-11-01 23:37:14 +0800

    music protoman whistle done

--- audio.asm
+++ audio.asm
@@ -21,5 +21,6 @@
 .INCLUDE "audio/music/password.asm"
 .INCLUDE "audio/music/staff_roll.asm"
 .INCLUDE "audio/music/stage_select.asm"
+.INCLUDE "audio/music/protoman_whistle.asm"
 .INCBIN  "audio/audio.bin"
 .INCLUDE "audio/unused/unused.asm"
