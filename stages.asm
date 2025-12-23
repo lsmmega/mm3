0e73edd8bc9f3ec9c77e7a9d53e2521976b1cd50
lsmmega <pogmegalsm@outlook.com>
2025-12-24 00:09:20 +0800

    split mapset

--- stages.asm
+++ stages.asm
@@ -1,64 +1,80 @@
 .SEGMENT "BANK00"
 .INCLUDE "constants/tsa.asm"
 .INCBIN  "stages/needleman/0.bin"
+.INCLUDE "stages/needleman/needleman_mapset.asm"
 .INCLUDE "stages/needleman/needleman_tsa.asm"
 
 .SEGMENT "BANK01"
 .INCBIN  "stages/magnetman/1.bin"
+.INCLUDE "stages/magnetman/magnetman_mapset.asm"
 .INCLUDE "stages/magnetman/magnetman_tsa.asm"
 
 .SEGMENT "BANK02"
 .INCBIN  "stages/geminiman/2.bin"
+.INCLUDE "stages/geminiman/geminiman_mapset.asm"
 .INCLUDE "stages/geminiman/geminiman_tsa.asm"
 
 .SEGMENT "BANK03"
 .INCBIN  "stages/hardman/3.bin"
+.INCLUDE "stages/hardman/hardman_mapset.asm"
 .INCLUDE "stages/hardman/hardman_tsa.asm"
 
 .SEGMENT "BANK04"
 .INCBIN  "stages/topman/4.bin"
+.INCLUDE "stages/topman/topman_mapset.asm"
 .INCLUDE "stages/topman/topman_tsa.asm"
 
 .SEGMENT "BANK05"
 .INCBIN  "stages/snakeman/5.bin"
+.INCLUDE "stages/snakeman/snakeman_mapset.asm"
 .INCLUDE "stages/snakeman/snakeman_tsa.asm"
 
 .SEGMENT "BANK06"
 .INCBIN  "stages/sparkman/6.bin"
+.INCLUDE "stages/sparkman/sparkman_mapset.asm"
 .INCLUDE "stages/sparkman/sparkman_tsa.asm"
 
 .SEGMENT "BANK07"
 .INCBIN  "stages/shadowman/7.bin"
+.INCLUDE "stages/shadowman/shadowman_mapset.asm"
 .INCLUDE "stages/shadowman/shadowman_tsa.asm"
 
 .SEGMENT "BANK08"
 .INCBIN  "stages/doc_needleman/8.bin"
+.INCLUDE "stages/doc_needleman/doc_needleman_mapset.asm"
 .INCLUDE "stages/doc_needleman/doc_needleman_tsa.asm"
 
 .SEGMENT "BANK09"
 .INCBIN  "stages/doc_geminiman/9.bin"
+.INCLUDE "stages/doc_geminiman/doc_geminiman_mapset.asm"
 .INCLUDE "stages/doc_geminiman/doc_geminiman_tsa.asm"
 
 .SEGMENT "BANK0A"
 .INCBIN  "stages/doc_sparkman/10.bin"
+.INCLUDE "stages/doc_sparkman/doc_sparkman_mapset.asm"
 .INCLUDE "stages/doc_sparkman/doc_sparkman_tsa.asm"
 
 .SEGMENT "BANK0B"
 .INCBIN  "stages/doc_shadowman/11.bin"
+.INCLUDE "stages/doc_shadowman/doc_shadowman_mapset.asm"
 .INCLUDE "stages/doc_shadowman/doc_shadowman_tsa.asm"
 
 .SEGMENT "BANK0C"
 .INCBIN  "stages/wily1/12.bin"
+.INCLUDE "stages/wily1/wily1_mapset.asm"
 .INCLUDE "stages/wily1/wily1_tsa.asm"
 
 .SEGMENT "BANK0D"
 .INCBIN  "stages/wily2_3_5/13.bin"
+.INCLUDE "stages/wily2_3_5/wily2_3_5_mapset.asm"
 .INCLUDE "stages/wily2_3_5/wily2_3_5_tsa.asm"
 
 .SEGMENT "BANK0F"
 .INCBIN  "stages/wily4/15.bin"
+.INCLUDE "stages/wily4/wily4_mapset.asm"
 .INCLUDE "stages/wily4/wily4_tsa.asm"
 
 .SEGMENT "BANK11"
 .INCBIN  "stages/wily6/17.bin"
+.INCLUDE "stages/wily6/wily6_mapset.asm"
 .INCLUDE "stages/wily6/wily6_tsa.asm"
---
+++ stages/wily4/wily4_mapset.asm
@@ -0,0 +1,319 @@
+;wily4_mapset_00
+	.BYTE $00, $01, $02, $03, $04, $05, $06, $07
+	.BYTE $00, $08, $09, $09, $0A, $0B, $0C, $07
+	.BYTE $00, $0D, $0D, $0E, $0F, $0D, $0D, $07
+	.BYTE $00, $10, $11, $12, $13, $14, $15, $07
+	.BYTE $00, $16, $17, $18, $19, $1A, $1B, $07
+	.BYTE $1C, $1C, $1C, $1C, $00, $1D, $07, $1C
+	.BYTE $1C, $1C, $1C, $1C, $00, $0F, $07, $1C
+	.BYTE $1C, $1C, $1C, $1C, $00, $1E, $07, $1C
+
+;wily4_mapset_01
+	.BYTE $1F, $1F, $1F, $1F, $20, $21, $22, $1F
+	.BYTE $20, $23, $24, $25, $26, $27, $28, $22
+	.BYTE $20, $29, $2A, $2B, $2C, $2D, $04, $22
+	.BYTE $20, $2E, $2F, $30, $31, $32, $33, $22
+	.BYTE $20, $34, $1E, $0E, $35, $0F, $36, $22
+	.BYTE $20, $37, $38, $1D, $39, $14, $3A, $22
+	.BYTE $20, $3B, $1F, $1F, $1F, $1F, $1F, $1F
+	.BYTE $20, $3C, $1F, $1F, $1F, $1F, $1F, $1F
+
+;wily4_mapset_02
+	.BYTE $00, $3D, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $00, $3E, $3F, $40, $41, $42, $43, $07
+	.BYTE $1C, $00, $0F, $44, $45, $46, $47, $07
+	.BYTE $00, $48, $49, $0D, $0D, $44, $34, $07
+	.BYTE $00, $1D, $0E, $4A, $4B, $14, $35, $07
+	.BYTE $00, $35, $1E, $1E, $1E, $37, $1E, $07
+	.BYTE $00, $4C, $4C, $07, $1C, $4D, $4E, $07
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $4F, $07
+
+;wily4_mapset_03
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $50, $22
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $51, $22
+	.BYTE $20, $01, $52, $53, $04, $05, $06, $22
+	.BYTE $20, $54, $0A, $55, $09, $56, $57, $22
+	.BYTE $20, $0F, $58, $59, $5A, $38, $15, $22
+	.BYTE $20, $35, $5B, $17, $18, $5C, $1B, $22
+	.BYTE $20, $3B, $1F, $1F, $1F, $1F, $1F, $1F
+	.BYTE $20, $3C, $1F, $1F, $1F, $1F, $1F, $1F
+
+;wily4_mapset_04
+	.BYTE $00, $5D, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $00, $5E, $1C, $1C, $00, $5F, $60, $07
+	.BYTE $00, $61, $62, $04, $05, $06, $63, $07
+	.BYTE $00, $64, $34, $34, $56, $65, $66, $07
+	.BYTE $00, $67, $68, $36, $0F, $09, $1E, $07
+	.BYTE $00, $17, $18, $69, $38, $1D, $6A, $07
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $4F, $07
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $4F, $07
+
+;wily4_mapset_05
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $50, $22
+	.BYTE $20, $6B, $6C, $6D, $1E, $6E, $6F, $22
+	.BYTE $20, $70, $71, $5B, $72, $22, $1F, $1F
+	.BYTE $20, $73, $22, $1F, $1F, $1F, $1F, $1F
+	.BYTE $20, $74, $75, $6E, $1E, $6D, $76, $77
+	.BYTE $20, $78, $79, $5B, $7A, $5B, $7B, $77
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $1F, $1F
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $1F, $1F
+
+;wily4_mapset_06
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $1F, $1F
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $1F, $1F
+	.BYTE $1F, $1F, $1F, $1F, $1F, $7C, $1F, $1F
+	.BYTE $1F, $1F, $1F, $1F, $20, $7D, $22, $1F
+	.BYTE $7E, $6D, $1E, $44, $1E, $7F, $22, $1F
+	.BYTE $80, $1D, $39, $14, $76, $81, $22, $1F
+	.BYTE $1F, $1F, $1F, $1F, $20, $82, $22, $1F
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $1F, $1F
+
+;wily4_mapset_07
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $1F, $1F
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $1F, $1F
+	.BYTE $1F, $1F, $1F, $1F, $1F, $1F, $1F, $1F
+	.BYTE $20, $83, $22, $1F, $1F, $1F, $1F, $1F
+	.BYTE $1F, $84, $1F, $1F, $1F, $1F, $1F, $1F
+	.BYTE $20, $81, $68, $1E, $85, $1F, $1F, $1F
+	.BYTE $20, $82, $22, $50, $85, $1F, $1F, $1F
+	.BYTE $1F, $1F, $1F, $50, $85, $1F, $1F, $1F
+
+;wily4_mapset_08
+	.BYTE $86, $87, $88, $89, $8A, $88, $8B, $8C
+	.BYTE $8D, $81, $8E, $8F, $90, $91, $81, $92
+	.BYTE $8D, $93, $94, $8F, $90, $95, $93, $92
+	.BYTE $8D, $81, $8E, $96, $97, $91, $81, $98
+	.BYTE $8D, $93, $8E, $99, $9A, $91, $93, $77
+	.BYTE $8D, $81, $1E, $81, $81, $1E, $81, $77
+	.BYTE $9B, $9C, $9D, $9C, $9C, $9D, $9C, $9E
+	.BYTE $9F, $9F, $9F, $9F, $9F, $9F, $9F, $9F
+
+;wily4_mapset_09
+	.BYTE $A0, $A1, $A1, $A1, $A1, $A1, $A1, $A2
+	.BYTE $8D, $7F, $A3, $A4, $A4, $A4, $A4, $92
+	.BYTE $8D, $81, $A5, $A6, $A6, $A6, $A6, $92
+	.BYTE $A7, $A8, $A9, $AA, $AB, $A6, $A6, $92
+	.BYTE $AC, $A4, $A4, $A4, $A4, $A6, $AD, $AE
+	.BYTE $AF, $A6, $A6, $A6, $A6, $A6, $92, $9F
+	.BYTE $B0, $B0, $B0, $B0, $B0, $B1, $B2, $9F
+	.BYTE $9F, $9F, $9F, $9F, $9F, $9F, $9F, $9F
+
+;wily4_mapset_0a
+	.BYTE $B3, $B4, $B4, $B4, $B4, $B4, $B4, $B5
+	.BYTE $B6, $A4, $A4, $A4, $A4, $A4, $A4, $B7
+	.BYTE $B8, $A6, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $B8, $A6, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $BA, $BB, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $B8, $A6, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $BC, $BD, $B0, $B0, $B0, $B0, $B0, $BE
+	.BYTE $BF, $C0, $9F, $9F, $9F, $9F, $9F, $9F
+
+;wily4_mapset_0b
+	.BYTE $B3, $B4, $B4, $B4, $B4, $B4, $B4, $B5
+	.BYTE $B6, $A4, $A4, $A4, $A4, $A4, $A4, $B7
+	.BYTE $B8, $A6, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $B8, $A6, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $B8, $A6, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $C1, $C2, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $C3, $C4, $C5, $C5, $C5, $C5, $C5, $C6
+	.BYTE $BF, $C0, $9F, $9F, $9F, $9F, $9F, $9F
+
+;wily4_mapset_0c
+	.BYTE $B3, $B4, $B4, $B4, $B4, $B4, $B4, $B5
+	.BYTE $B6, $A4, $A4, $A4, $A4, $A4, $A4, $B7
+	.BYTE $B8, $A6, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $B8, $A6, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $BA, $BB, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $B8, $A6, $A6, $C7, $C8, $A6, $A6, $B9
+	.BYTE $BC, $BD, $C9, $CA, $CB, $CC, $B0, $BE
+	.BYTE $BF, $C0, $9F, $9F, $9F, $9F, $9F, $9F
+
+;wily4_mapset_0d
+	.BYTE $B3, $B4, $B4, $B4, $B4, $B4, $B4, $B5
+	.BYTE $B6, $A4, $A4, $A4, $A4, $A4, $A4, $B7
+	.BYTE $B8, $A6, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $B8, $A6, $A6, $A6, $A6, $A6, $A6, $B9
+	.BYTE $C1, $C2, $A6, $C7, $C8, $A6, $A6, $B9
+	.BYTE $C3, $C4, $CD, $BE, $CE, $C8, $A6, $B9
+	.BYTE $BF, $C0, $9F, $9F, $9F, $CE, $B0, $BE
+	.BYTE $BF, $C0, $9F, $9F, $9F, $9F, $9F, $9F
+
+;wily4_mapset_0e
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_0f
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_10
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_11
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_12
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_13
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_14
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_15
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_16
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_17
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_18
+	.BYTE $CF, $D0, $88, $89, $8A, $88, $D1, $D2
+	.BYTE $D3, $D4, $D5, $D6, $0E, $D7, $D8, $D9
+	.BYTE $DA, $DB, $DC, $DD, $DE, $14, $DF, $E0
+	.BYTE $D3, $D4, $E1, $E2, $D5, $E3, $D8, $E4
+	.BYTE $DA, $E5, $E6, $E7, $E7, $E8, $E9, $EA
+	.BYTE $D3, $D4, $EB, $81, $81, $EC, $D8, $ED
+	.BYTE $EE, $EF, $F0, $9C, $9C, $9E, $F1, $EF
+	.BYTE $9F, $9F, $9F, $9F, $9F, $9F, $9F, $9F
+
+;wily4_mapset_19
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_1a
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_1b
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_1c
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_1d
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_1e
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+
+;wily4_mapset_1f
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
+	.BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E
