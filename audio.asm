50633df53f19ba9ada2e121252ec66463b9c7a6d
lsmmega <pogmegalsm@outlook.com>
2025-12-26 20:23:05 +0800

    sfx enemy hit done

--- audio.asm
+++ audio.asm
@@ -28,6 +28,7 @@
 .INCLUDE "audio/sfx/mega_buster.asm"
 .INCLUDE "audio/sfx/megaman_hit.asm"
 .INCLUDE "audio/sfx/death.asm"
+.INCLUDE "audio/sfx/enemy_hit.asm"
 .INCBIN  "audio/audio_1.bin"
 .INCLUDE "audio/music/boss_show.asm"
 .INCBIN  "audio/audio_2.bin"
---
+++ audio/sfx/enemy_hit.asm
@@ -0,0 +1,17 @@
+sfx_enemy_hit:
+	sfx_priority 9
+
+;1
+	current_sfx_global_transpose .SET 0
+	sfx_global_channel_flags sfx_ads_set
+	sfx_ads $C8
+	sfx_frames 28
+	.BYTE sfx_noise
+
+;sfx_noise
+	sfx_channel_flags sfx_instruments_set | sfx_volume_set | sfx_pitch_slide_set
+	sfx_instruments 50
+	sfx_volume 15
+	sfx_pitch_slide $FF
+	sfx_noise_note 13
+	sfx_end
