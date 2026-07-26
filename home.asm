8bc80d8ffacbd878ce2f9600b937a0145cefc588
lsmmega <pogmegalsm@outlook.com>
2026-07-26 23:09:44 +0800

    add screen

--- home.asm
+++ home.asm
@@ -1,6 +1,7 @@
 .SEGMENT "HOME"
 .INCLUDE "constants/button.asm"
 .INCBIN  "home/home_1_1.bin"
+.INCLUDE "home/screen.asm"
 .INCLUDE "home/render.asm"
 .INCLUDE "home/joy.asm"
 .INCLUDE "home/init_ppu.asm"
